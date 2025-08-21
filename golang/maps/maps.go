2d822b8adcf0d025c643eaaea9b8c0936f0baf17
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-08-21 18:58:48 +0530

    test case 3

--- golang/maps/maps.go
+++ golang/maps/maps.go
@@ -1,1 +1,17 @@
 package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+func (D Dictionary) Search(word string) (string,error) {
+
+	dictionary, ok := D[word]
+
+	if !ok {
+		return "",errors.New("could not find the word here")
+	}
+
+	return dictionary, nil
+}
+
--- golang/maps/maps_test.go
+++ golang/maps/maps_test.go
@@ -1,1 +1,37 @@
 package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	dictionary := Dictionary{"test" : "this is test 1"}
+
+	t.Run("known word",func(t *testing.T) {
+
+		got,_ := dictionary.Search("test")
+		want := "this is test 1"
+
+		assertStrings(t,got,want)
+	})
+
+	t.Run("unknown word",func(t *testing.T){
+		_,err := dictionary.Search("tasty")
+		
+		want := "could not find the word here"
+
+
+		if err == nil {
+          t.Fatal("expected an error here.")
+		}
+
+		assertStrings(t,err.Error(),want)
+	})
+}
+
+func assertStrings(t testing.TB,got string,want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given %q",got , want , "test")
+	}
+}
+
